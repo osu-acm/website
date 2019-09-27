0964791de44d722daed87067373a87bff36215a3
joeking9292 <noonanjoe92@gmail.com>
2019-09-27 16:36:28 -0700

    Start to the server

--- newOSUACMWebsite/server.js
+++ newOSUACMWebsite/server.js
@@ -2,3 +2,14 @@
  * Author: Joseph Noonan
  * Date: 9/27/19
  */
+
+var path = require('path');
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 3500;
+
+app.use(express.static('public'));
+
+app.get(['/', '/homePage.html'], function(req, res, next) {
+    res.status(200).render('homePage');
+});
