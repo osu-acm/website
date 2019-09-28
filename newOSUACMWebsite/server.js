448aaa24261d06cec4d586bbfb4cfb71ce59bc11
joeking9292 <noonanjoe92@gmail.com>
2019-09-27 17:17:37 -0700

    Not express server

--- newOSUACMWebsite/server.js
+++ newOSUACMWebsite/server.js
@@ -3,13 +3,64 @@
  * Date: 9/27/19
  */
 
-var path = require('path');
-var express = require('express');
-var app = express();
+var http = require('http');
+var fs = require('fs');
 var port = process.env.PORT || 3500;
 
-app.use(express.static('public'));
+var server = http.createServer(function (req, res) {
+    res.statusCode = 200;
+    switch(req.url){
+        case "/homePage":
+            res.end(fs.readFileSync("homePage.html"));
+            console.log("== loaded homePage.html");
+            break;
+        case "/officerPage":
+            res.end(fs.readFileSync("officerPage.html"));
+            console.log("== loaded officerPage.html");
+            break;
+        case "/constitutionPage":
+            res.end(fs.readFileSync("constitutionPage.html"));
+            console.log("== loaded constitutionPage.html");
+            break;
+        case "/aboutPage":
+            res.end(fs.readFileSync("aboutPage.html"));
+            console.log("== loaded aboutPage.html");
+            break;
+        case "/header.css":
+            res.end(fs.readFileSync("header.css"));
+            console.log("== loaded header.css");
+            break;
+        case "/":
+            res.end(fs.readFileSync("homePage.html"));
+            console.log("== loaded homePage.html");
+            break;
+        case "/homePage.css":
+                res.end(fs.readFileSync("homePage.css"));
+                console.log("== loaded homePage.css");
+                break;
+        case "/officerPage.css":
+            res.end(fs.readFileSync("officerPage.css"));
+            console.log("== loaded officerPage.css");
+            break;
+        case "/constitutionPage.css":
+            res.end(fs.readFileSync("constitutionPage.css"));
+            console.log("== loaded constitutionPage.css");
+            break;
+        case "/aboutPage.css":
+            res.end(fs.readFileSync("aboutPage.css"));
+            console.log("== loaded aboutPage.css");
+            break;             
+        default:
+            res.end(fs.readFileSync("404.html"));
+            res.statusCode = 404;
+            console.log("== loaded 404.html");
+            break;
+    }
+});
 
-app.get(['/', '/homePage.html'], function(req, res, next) {
-    res.status(200).render('homePage');
+server.listen(port, function(err) {
+    if (err) {
+        throw err;
+    }
+    console.log("== Server is listening on port", port);
 });
