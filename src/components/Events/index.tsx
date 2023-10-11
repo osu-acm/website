36d4f3fc19f57dc726eeffa3a7c4cd907d250823
Tom Nyuma <46255836+Nyumat@users.noreply.github.com>
2023-10-11 15:49:17 -0700

    👀 Added Week 2 Event (#23)

--- src/components/Events/index.tsx
+++ src/components/Events/index.tsx
@@ -1,7 +1,40 @@
 // Styles
 import "./style.css";
 
+interface EventProps {
+  date: string;
+  time: string;
+  location: string;
+  title: string;
+  description: string;
+}
+
+function Event({ date, time, location, title, description }: EventProps) {
+  return (
+    <div className="event">
+      <span className="event-date">{date}</span>
+      <div className="event-info">
+        <h3 className="event-info-title">{title}</h3>
+        <p className="event-info-time">{time}</p>
+        <p className="event-info-location">{location}</p>
+        <p className="event-info-description">{description}</p>
+      </div>
+    </div>
+  );
+}
+
 const Events = ({ haveEvents }: { haveEvents: boolean }) => {
+  const events = [
+    {
+      date: "Thursday, October 12, 2023",
+      time: "6:00 PM - 7:00 PM",
+      location: "Kelley Engineering Center Room 1003 & Discord",
+      title: "Introduction To Competitive Programming w/ Otso and Nick",
+      description:
+        "For this Thursday’s meeting, we will be getting everyone up to speed on competitive programming! This is completely beginner friendly, no experience is required!",
+    },
+  ];
+
   return (
     <section className="events">
       <div className="container">
@@ -24,6 +57,20 @@ const Events = ({ haveEvents }: { haveEvents: boolean }) => {
               </div>
             )}
 
+            {haveEvents && (
+              <div className="events-list">
+                {events.map((event, index) => (
+                  <Event
+                    key={index}
+                    date={event.date}
+                    time={event.time}
+                    location={event.location}
+                    title={event.title}
+                    description={event.description}
+                  />
+                ))}
+              </div>
+            )}
           </div>
         </div>
       </div>
